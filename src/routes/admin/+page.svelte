c9a7aa1a8f8a19f4392e019db02fea7ba79c04fd
Noureddine Sidi Abed <noureddinesidiabed6k@gmail.com>
2026-01-06 20:17:41 -0500

    created admin route, created remote function to grab all posts and display on the page

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { getAllPosts } from './posts.remote';
+</script>
+
+<ol>
+	{#each await getAllPosts() as post (post.id)}
+		<li>{post.title}</li>
+		<li>{post.slug}</li>
+		<li>{post.body}</li>
+		<li>{post.createdAt}</li>
+		<li>{post.updatedAt}</li>
+	{/each}
+</ol>
+
+<!-- List all posts -->
